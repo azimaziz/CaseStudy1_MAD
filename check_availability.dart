232a0ff7afba92c401af46b25d81a0f567af5bff
azimaziz <57623687+azimaziz@users.noreply.github.com>
2023-11-24 23:42:03 +0800

    Add files via upload

---
+++ check_availability.dart
@@ -0,0 +1,110 @@
+import 'dart:html';
+import 'dart:convert';
+
+void main() {
+  var form1 = querySelector('#availabilityForm') as FormElement;
+  form1.onSubmit.listen((Event e) {
+    e.preventDefault();
+
+    var selectedVenue = (querySelector('#venue') as SelectElement).value ?? '';
+    var selectedDate = (querySelector('#date') as InputElement).value ?? '';
+
+    clearTable();
+    checkAvailability(selectedVenue, selectedDate);
+  });
+}
+
+void checkAvailability(String venue, String date) {
+  var storedEvents = window.localStorage['events'];
+  bool isAvailable = true;
+
+  if (storedEvents != null) {
+    var events = (json.decode(storedEvents) as List<dynamic>)
+        .map((e) => Map<String, String>.from(e as Map))
+        .toList();
+
+    for (var event in events) {
+      if (event['eventVenue'] == venue && event['date'] == date) {
+        isAvailable = false;
+        break;
+      }
+    }
+  }
+
+  // Display the availability status
+  var outputElement = querySelector('#availabilityOutput') as DivElement;
+  if (isAvailable) {
+    outputElement.text = 'The venue $venue is available on $date.';
+  } else {
+    outputElement.text = 'The venue $venue is booked on $date.';
+    loadEvents(venue, date);
+  }
+}
+
+void clearTable() {
+  var table = querySelector('.centered-table') as TableElement;
+  var rows = table.rows;
+  for (var i = rows.length - 1; i >= 1; i--) {
+    rows[i].remove();
+  }
+
+  var outputElement = querySelector('#suggestText') as DivElement;
+  outputElement.text = '';
+}
+
+void loadEvents(venue, date) {
+  var outputElement = querySelector('#suggestText') as DivElement;
+  outputElement.text = 'These are the schedule for venue $venue 3 days before & after $date.';
+
+  for (int i = 3; i >= 1; i--) {
+    var previousDate = DateTime.parse(date).subtract(Duration(days: i)).toString();
+    buildTable(venue, previousDate);
+  }
+
+  for (int i = 1; i <= 3; i++) {
+    var nextDate = DateTime.parse(date).add(Duration(days: i)).toString();
+    buildTable(venue, nextDate);
+  }
+}
+
+void buildTable(venue, newDate) {
+  var storedEvents = window.localStorage['events'];
+  var table = querySelector('.centered-table') as TableElement;
+  bool isAvailable = false;
+
+  if (storedEvents != null) {
+    var events = (json.decode(storedEvents) as List<dynamic>)
+        .map((e) => Map<String, String>.from(e as Map))
+        .toList();
+
+    for (var event in events) {
+      print(event['date']);
+      print(newDate);
+
+      if (event['eventVenue'] == venue) {
+        if (DateTime.parse(event['date']!) == DateTime.parse(newDate)) {
+          var row = table.addRow();
+          row.addCell().text = event['personName'] ?? '';
+          row.addCell().text = event['eventName'] ?? '';
+          row.addCell().text = event['eventVenue'] ?? '';
+          row.addCell().text = event['date'] ?? '';
+          row.addCell().text = event['time'] ?? '';
+          isAvailable = true;
+        } else {}
+      } else {}
+    }
+    if (isAvailable == false) {
+      var row = table.addRow();
+      row.addCell().text = '  -  ';
+      row.addCell().text = 'Date Available';
+      row.addCell().text = venue ?? '';
+      row.addCell().text = abbrevBad(newDate);
+      row.addCell().text = '  -  ';
+    } else {}
+  }
+}
+
+String abbrevBad(String input) {
+  if (input.length <= 10) return input;
+  return input.substring(0, 10);
+}
