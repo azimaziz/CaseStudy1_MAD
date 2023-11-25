cb4d4d6976f4b370f06006274724057b8a6ba3b0
Haikal Ritz <136175182+Damerial@users.noreply.github.com>
2023-11-25 12:56:45 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 
 void main() {
   print("Dart script loaded and running.");
+  setMinDateForEvent();
+  
   var form = querySelector('#scheduleEvent') as FormElement;
 
   form.onSubmit.listen((Event e) {
@@ -59,4 +61,11 @@ void saveEvent(Map<String, String> event) {
   List<Map<String, String>> events = storedEvents != null ? (json.decode(storedEvents) as List).map((e) => Map<String, String>.from(e as Map)).toList() : [];
   events.add(event);
   window.localStorage['events'] = json.encode(events);
+}
+
+void setMinDateForEvent() {
+  var dateInput = querySelector('#date') as InputElement;
+  var today = DateTime.now();
+  var dateString = "${today.year.toString().padLeft(4, '0')}-${today.month.toString().padLeft(2, '0')}-${today.day.toString().padLeft(2, '0')}";
+  dateInput.setAttribute('min', dateString);
 }
